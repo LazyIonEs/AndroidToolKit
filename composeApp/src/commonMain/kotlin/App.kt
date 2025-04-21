2a0d698d94ff9de35844bf80d02efd2887345115
LazyIonEs <lazyiones@qq.com>
2025-04-21 09:48:57 +0800

    优化、CMP升级至1.8.0-beta02、filekit升级至0.10.0-beta02

--- composeApp/src/commonMain/kotlin/App.kt
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -38,7 +38,6 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.SnackbarResult
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TooltipBox
 import androidx.compose.material3.rememberTooltipState
@@ -102,9 +101,7 @@ fun App() {
         DarkThemeConfig.FOLLOW_SYSTEM -> isSystemInDarkTheme()
     }
     AppTheme(useDarkTheme) {
-        Surface {
-            MainContentScreen(viewModel)
-        }
+        MainContentScreen(viewModel)
     }
 }
 
